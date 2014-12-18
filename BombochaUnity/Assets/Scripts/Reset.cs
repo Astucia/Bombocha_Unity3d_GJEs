1f23622ad789b2ecfe7879fc3d67bf495a31ac22
alex31016 <alex31016@gmail.com>
2014-12-18 01:12:26 -0600

    Prototipo en Unity 2
    Archivos que faltaron

---
+++ BombochaUnity/Assets/Scripts/Reset.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ResetPrototype(){
+		Application.LoadLevel(0);
+	}
+}
---
+++ BombochaUnity/Assets/Scripts/Shoot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject ball;
+	public int force;
+	private bool isMoving = false;
+
+	void FixedUpdate(){
+		if(Input.GetButtonDown("Jump") && !isMoving){
+			//Instantiate(objects[Random.Range(0, objects.Length)], transform.position, Quaternion.identity);
+			isMoving = true;
+			ball.rigidbody.AddForce(transform.parent.forward * force);
+			ball.transform.parent = null;
+			ball.rigidbody.useGravity = true;
+		}
+	}
+}
