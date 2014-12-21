00276dce47817ce29968b625681d942dc8713c60
Astucia <soporte@lulamb.com>
2014-12-21 14:23:20 -0600

    Se agregaron archivos de música
    Se agregaron archivos de música y se colocaron en el menú y en el juego.

---
+++ BombochaUnity/Assets/Scripts/MenuMusik.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusik : MonoBehaviour {
+
+    GameObject Camara = GameObject.Find("Main Camera");
+
+    void Awake()
+    {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        Object.DontDestroyOnLoad(this);
+        if (!this.audio.isPlaying)
+        {
+            this.audio.Play();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Application.loadedLevelName == "Prototype" && this.audio.isPlaying)
+        {
+            this.audio.Stop();
+        }
+        //else
+        //{
+        //    this.transform.position = Camara.transform.position;
+        //}
+	}
+}
